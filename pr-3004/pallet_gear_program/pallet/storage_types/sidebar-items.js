56b3961e0ec00309601d3f4cd6f303c88493405b
StackOverflowExcept1on <StackOverflowExcept1on@users.noreply.github.com>
2023-07-30 13:41:31 +0000

    deploy: 4676f8bbf5c44fd8e109a05e9cdf4cd2ed530812

---
+++ pr-3004/pallet_gear_program/pallet/storage_types/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["CodeLenStorage","CodeStorage","MemoryPageStorage","MetadataStorage","OriginalCodeStorage","PausedProgramStorage","ProgramStorage","ResumeSessions","ResumeSessionsNonce","SessionMemoryPages","WaitingInitStorage"]};
